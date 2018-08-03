8b338bc7327e270c646a4502799766db4422f990
James <jjlong@mit.edu>
2018-08-03 16:35:38 -0400

    solar q learner to cloud

---
+++ solar_qlearner.py
@@ -0,0 +1,14 @@
+import qlearner
+import gym
+from gym.envs.registration import registry, register, make, spec
+
+if __name__=='__main__':
+    register(
+    id='SolarSensor-v0',
+    entry_point='solar_sensor_env:SolarSensorEnv',
+    kwargs = {'max_batt':100,'num_sensors':1}
+    )
+    env = gym.make('SolarSensor-v0')
+    qagent = qlearner.QLearner(env, n_episodes=10000, min_alpha=0.01, min_epsilon=0.01,
+                      ada_divisor=30, gamma=0.95,max_env_steps=28*48)
+    qagent.run()
