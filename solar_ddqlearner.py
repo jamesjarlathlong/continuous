cba7b6f17ec75ebba1c7811e6f75ddd875ee4211
James <jjlong@mit.edu>
2018-09-13 16:45:37 -0400

    using real solar

--- solar_ddqlearner.py
+++ solar_ddqlearner.py
@@ -5,6 +5,7 @@ import os
 import string
 import random
 import simple_solar_env
+import solar_sensor_env
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import sys
 if __name__=='__main__':
@@ -12,7 +13,9 @@ if __name__=='__main__':
     phase = sys.argv[2]
     recordname = '_'.join([modeldir,phase])
     print('experiment id:{}'.format(recordname))
-    solarrecord = simple_solar_env.emulate_solar_ts(365)
+    #solarrecord = simple_solar_env.emulate_solar_ts(365)
+    solarfname = 'training_12'
+    solarrecord = solar_sensor_env.get_generated_power(solarfname)
     register(
     id='SolarSensor-v0',
     entry_point='solar_sensor_env:SolarSensorEnv',
