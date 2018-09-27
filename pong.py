307b0edf5d02b26a13cb46dbd1a9520fdaa8d384
James <jjlong@mit.edu>
2018-09-27 05:04:55 -0400

    flatten state error in argument corrected

--- pong.py
+++ pong.py
@@ -63,7 +63,7 @@ def policy_backward(model, episode_h, episode_dlogp, episode_states):
   dW1 = episode_states.T.dot(dh)#np.dot(dh.T, episode_states)
   return {'W1':dW1, 'W2':dW2}
 
-def flatten_state(state):
+def flatten_state(I):
     """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
     I = I[35:195] # crop
     I = I[::2,::2,0] # downsample by factor of 2
@@ -157,5 +157,5 @@ class PgLearner():
         return e
 if __name__ == '__main__':
     env = gym.make("Pong-v0")
-    pgagent = PgLearner(learning_rate = 1e-4, modeldir='tmp/pong6', n_episodes=500,gamma=0.99, batch=1)
+    pgagent = PgLearner(env,learning_rate = 1e-4, modeldir='tmp/pong6', n_episodes=500,gamma=0.99, batch=1)
     pgagent.run()
