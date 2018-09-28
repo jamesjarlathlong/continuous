2cebb7e2a868b5b802e22104ef69fda5b61b4c7f
jjl <jjlong@mit.edu>
2018-09-28 13:15:58 -0400

    running long

--- solar_pglearner_multi.py
+++ solar_pglearner_multi.py
@@ -23,6 +23,6 @@ if __name__=='__main__':
     kwargs = {'max_batt':10,'num_sensors':1,'deltat':3, 'solarpowerrecord':solarrecord, 'recordname':recordname}
     )
     env = gym.make('SolarSensor-v0')
-    pgagent = np_pg.PgLearner(env, learning_rate = 1e-2, modeldir=savemodel, n_episodes=5000,gamma=0.99, batch=5,max_env_steps=28*8)
+    pgagent = np_pg.PgLearner(env, learning_rate = 1e-4, modeldir=savemodel, n_episodes=5000,gamma=0.99, batch=15,max_env_steps=28*8)
     pgagent.run()
     #agent.model.save('tmp/{}'.format(modeldir))
