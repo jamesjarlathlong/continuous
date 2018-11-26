e5cf61a636ea64c20d8c7864c530cfe0b997f041
jjl <jjlong@mit.edu>
2018-11-26 13:12:04 -0500

    updating to gridworld

--- solar_ddqlearner_september.py
+++ solar_ddqlearner_september.py
@@ -26,17 +26,17 @@ if __name__=='__main__':
     solarrecord = simple_solar_env.emulate_solar_ts(365)
     solarfname = 'training_12'
     solarrecord = solar_sensor_env.get_generated_power(solarfname)
-    monthrecord = get_month(solarrecord,0)
+    monthrecord = get_month(solarrecord,8)
     register(
     id='SolarGraphSensor-v0',
     entry_point='solar_sensor_env:SolarGraphSensorEnv',
     kwargs = {'max_batt':10,'num_sensors':num_sensors, 'deltat':3,'solarpowerrecord':monthrecord, 'recordname':recordname}
     )
     env = gym.make('SolarGraphSensor-v0')
-    agent = ddqlearner.DDQNAgent(env,n_episodes = 2000, max_env_steps=300*8, modeldir=loadmodel,decay_rate = 0.999999, learning_rate = learning_rate, layer_width=layer_width)
-    #agent = ddqlearner.DDQNAgent(env,n_episodes = 2000, max_env_steps=300*8, modeldir=loadmodel,decay_rate = 0.01, learning_rate = learning_rate, layer_width=layer_width)
+    agent = ddqlearner.DDQNAgent(env,n_episodes = 5000, max_env_steps=300*8, modeldir=loadmodel,decay_rate =0.9999995, learning_rate = learning_rate, layer_width=layer_width)
+    #agent = ddqlearner.DDQNAgent(env,n_episodes = 500, max_env_steps=300*8, modeldir=loadmodel,decay_rate = 0.9, learning_rate = learning_rate, layer_width=layer_width)
 
-    #agent.run()
-    #agent.model.save('tmp/{}'.format(modeldir))
-    naiveagent = simple_agent.SimpleNetworkAgent(env, n_episodes = 10, max_env_steps = 300*8, num_on=8)
-    naiveagent.run()
+    agent.run()
+    agent.model.save('tmp/{}'.format(modeldir))
+    #naiveagent = simple_agent.SimpleNetworkAgent(env, n_episodes = 10, max_env_steps = 300*8, num_on=8)
+    #naiveagent.run()
