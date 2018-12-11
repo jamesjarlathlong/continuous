8ac669bba853560bcce76ebbaff596a815b18a63
jjl <jjlong@mit.edu>
2018-12-11 02:37:34 -0500

    conv working

--- ddqlearner.py
+++ ddqlearner.py
@@ -3,7 +3,7 @@ import gym
 import numpy as np
 from collections import deque
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Conv1D, Activation, Flatten
 from keras.optimizers import Adam
 from keras import backend as K
 from keras.models import load_model
@@ -23,6 +23,11 @@ def flatten_state(statedict):
     vals = [statedict[k][0::] for k in sorted(statedict)]
     flatvals = list(itertools.chain(*vals))
     return np.reshape(flatvals, [1,len(flatvals)])
+def flatten_state_forconv(statedict):
+    vals = [statedict[k][0::] for k in sorted(statedict)]
+    return np.asarray(vals)
+    #flatvals = list(itertools.chain(*vals))
+    #return np.reshape(flatvals, [1,len(flatvals)])
 def flatten_state_withtime(statedict):
     time = statedict['S0'][3]
     #month = statedict['S0'][4]
@@ -156,7 +161,7 @@ class DDQNConvAgent:
     def __init__(self, env,n_episodes, max_env_steps=None, modeldir=None, learning_rate = 0.0001, decay_rate = 0.999995, layer_width=64):
         self.env = env
         self.n_episodes = n_episodes
-        self.state_size = len(flatten_state_withtime(env.observation_space.sample())[0])
+        self.state_size = np.shape(flatten_state_forconv(env.observation_space.sample()))
         self.action_size = get_alt_action_size(self.env)
         print(self.action_size, self.state_size)
         self.action_lookup = functools.partial(alt_action_lookup,env)#list(itertools.product(*(range(space.n) for space in env.action_space.spaces)))
@@ -183,11 +188,13 @@ class DDQNConvAgent:
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        model.add(Convolution1D(self.layer_width, 4, input_shape = self.state_size, border_mode='same'))
+        print(self.state_size, 'state size')
+        model.add(Conv1D(self.layer_width, 4, input_shape = (16,4), padding='valid'))
         model.add(Activation('relu'))
-        model.add(Convolution1D(self.layer_width, 2, border_mode='same'))
+        model.add(Conv1D(self.layer_width, 2, padding='valid'))
         model.add(Activation('relu'))
         #model.add(Dense(128, activation='relu'))
+        model.add(Flatten())
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss=self._huber_loss,
                       optimizer=Adam(lr=self.learning_rate))
@@ -198,20 +205,24 @@ class DDQNConvAgent:
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
     def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
+        #if np.random.rand() <= self.epsilon:
+            #return random.randrange(self.action_size)
+        act_values = self.model.predict(state.reshape(1,16,4))
         return np.argmax(act_values[0])  # returns action
     def replay(self, batch_size):
         minibatch = random.sample(list(self.memory), batch_size)
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
+                shape = np.shape(next_state)
+                #shaped = np.reshape(next_state, (1,16,4))
+                #print(np.shape(shaped))
+                target_pred = self.target_model.predict(next_state.reshape(1,16,4))
                 target = reward + self.gamma * \
-                       np.amax(self.target_model.predict(next_state)[0])
-            target_f = self.model.predict(state)
+                       np.amax(target_pred[0])
+            target_f = self.model.predict(state.reshape(1,16,4))
             target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+            self.model.fit(state.reshape(1,16,4), target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
     def run(self, render=True):
@@ -219,17 +230,18 @@ class DDQNConvAgent:
             print('#######New episode#############', self.epsilon)
             done=False
             observation = self.env.reset()
-            prev_state = flatten_state_withtime(observation)
+            prev_state = flatten_state_forconv(observation)
             reward_sum = 0
             i=0
             while not done and i<self.env._max_episode_steps:
                 #print(i)
                 if render: self.env.render()
+                print(np.shape(prev_state))
                 action = self.act(prev_state)
                 # record various intermediates (needed later for backprop)
                 # step the environment and get new measurements
                 observation, reward, done, info = self.env.step(self.action_lookup(observation, action))
-                flat_state = flatten_state_withtime(observation)
+                flat_state = flatten_state_forconv(observation)
                 # Remember the previous state, action, reward, and done
                 self.remember(prev_state, action, reward, flat_state, done)
                 # make next_state the new current state for the next frame.
